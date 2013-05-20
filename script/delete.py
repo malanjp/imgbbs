3e834de418e09cbd7ba6a216dc716a540ef7342b
mshibata <shibata.michihiro@gmail.com>
2013-05-20 14:55:37 +0900

    celery task

--- script/delete.py
+++ script/delete.py
@@ -1,64 +1,76 @@
 # -*- coding: utf-8 -*-
 import os, sys
-from datetime import datetime
+import socket
+from datetime import datetime, timedelta
 import pymysql
 import pymysql.cursors
 
-current_path = os.path.abspath(os.path.dirname(__file__))
+from celery import Celery
+from celery.task import periodic_task
+
+celery = Celery('tasks', broker='amqp://guest@localhost//')
 
 def session():
-    return pymysql.connect(db='imgbbs', user='imgbbs', passwd='_WioT.A', charset='utf8')
+    if socket.gethostname() == 'mshibata-vm-ubuntu':
+        return pymysql.connect(db='imgbbs', host='192.168.72.100', user='imgbbs', passwd='_WioT.A', charset='utf8')
+    else:
+        return pymysql.connect(db='imgbbs', host='localhost', user='imgbbs', passwd='_WioT.A', charset='utf8')
 
-
-con = session()
-db = con.cursor(pymysql.cursors.DictCursor)
 
 # 親に連なるエントリ削除
-db.execute("select * from upimage where deltime <= now()")
-for row in db.fetchall():
-    if row.get('img'):
-        try:
-            os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('img')))
-            os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('thumb')))
-        except FileNotFoundError as e:
-            print(e)
+@periodic_task(run_every=timedelta(minutes=1))
+def delete():
 
-    # 子画像削除
-    query = "select * from reply where parent_id = %s" % row.get('id')
-    db.execute(query)
-    for child_row in db.fetchall():
-        if child_row.get('img'):
+    current_path = os.path.abspath(os.path.dirname(__file__))
+
+    con = session()
+    db = con.cursor(pymysql.cursors.DictCursor)
+    
+    db.execute("select * from upimage where deltime <= now()")
+    for row in db.fetchall():
+        if row.get('img'):
             try:
-                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', child_row.get('img')))
-                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', child_row.get('thumb')))
+                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('img')))
+                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('thumb')))
             except FileNotFoundError as e:
                 print(e)
-
-        # 子エントリ削除
-        query = "delete from reply where id = %s " % child_row.get('id')
+    
+        # 子画像削除
+        query = "select * from reply where parent_id = %s" % row.get('id')
         db.execute(query)
-
-    # 親エントリ削除
-    query = "delete from upimage where id = %s " % row.get('id')
-    db.execute(query)
-
-    con.commit()
-
-
-# 子エントリのみ削除
-db.execute("select * from reply where deltime <= now()")
-for row in db.fetchall():
-    if row.get('img'):
-        try:
-            os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('img')))
-            os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('thumb')))
-        except FileNotFoundError as e:
-            print(e)
-
-    # エントリ削除
-    query = "delete from reply where id = %s " % row.get('id')
-    db.execute(query)
-    con.commit()
+        for child_row in db.fetchall():
+            if child_row.get('img'):
+                try:
+                    os.remove(os.path.join(current_path, '../', 'contents/static/upload/', child_row.get('img')))
+                    os.remove(os.path.join(current_path, '../', 'contents/static/upload/', child_row.get('thumb')))
+                except FileNotFoundError as e:
+                    print(e)
+    
+            # 子エントリ削除
+            query = "delete from reply where id = %s " % child_row.get('id')
+            db.execute(query)
+    
+        # 親エントリ削除
+        query = "delete from upimage where id = %s " % row.get('id')
+        db.execute(query)
+    
+        con.commit()
+    
+    
+    # 子エントリのみ削除
+    db.execute("select * from reply where deltime <= now()")
+    for row in db.fetchall():
+        if row.get('img'):
+            try:
+                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('img')))
+                os.remove(os.path.join(current_path, '../', 'contents/static/upload/', row.get('thumb')))
+            except FileNotFoundError as e:
+                print(e)
+    
+        # エントリ削除
+        query = "delete from reply where id = %s " % row.get('id')
+        db.execute(query)
+        con.commit()
 
 
 
